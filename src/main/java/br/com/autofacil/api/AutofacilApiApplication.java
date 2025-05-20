79e8ae09b8cc28ba2f1afd2bfdeefb35a58ab7e5
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-05-20 10:06:37 -0300

    First commit

---
+++ src/main/java/br/com/autofacil/api/AutofacilApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.autofacil.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofacilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofacilApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/autofacil/api/AutofacilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autofacil.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofacilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
