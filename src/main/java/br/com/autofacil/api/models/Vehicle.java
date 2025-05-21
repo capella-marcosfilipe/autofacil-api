f9fd3c3e4ad6836cca6c1e823fb643b12c8ade52
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-05-21 09:59:10 -0300

    Criei os models para User, Vehicle e VendorSale.

--- src/main/java/br/com/autofacil/api/models/Vehicle.java
+++ src/main/java/br/com/autofacil/api/models/Vehicle.java
@@ -1,4 +1,140 @@
 package br.com.autofacil.api.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicles")
 public class Vehicle {
+    @Setter
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "vehicle_type")
+    private String vehicleType;
+
+    private String model;
+    private String brand;
+    private String color;
+    private Float price;
+
+    @Column(name = "fuel_type")
+    private String fuelType;
+
+    @Column(name = "transmission_type")
+    private String transmissionType;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "vehicle")
+    private List<VendorSale> vendorSales;
+
+    public List<VendorSale> getVendorSales() {
+        return vendorSales;
+    }
+
+    public void setVendorSales(List<VendorSale> vendorSales) {
+        this.vendorSales = vendorSales;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
